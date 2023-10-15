54683527be0b5e761de8843a4ef1329860f655d9
alshchetinin <alexsander.qw@gmail.com>
2023-10-15 12:11:26 +0800

    Revert "prod"
    This reverts commit 7e8190a0502c65ceb379674d513c2d75fb6ac641.

--- prod/_nuxt/Markdown.8cbf96c4.js
+++
@@ -1,1 +0,0 @@
-import r from"./ContentSlot.5a8439b2.js";import{f as o,C as f,g as u,L as c}from"./entry.221ab5ec.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:a}=f(),s=u(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
--- prod/_nuxt/ProseCode.50b950b3.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./ProseCode.vue.9f47c812.js";import"./entry.221ab5ec.js";export{m as default};
